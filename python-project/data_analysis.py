5391acdacc6fa440eb73a72dbdce34b7bd9d1bde
ASTRID DCOSTA <astdcost@publicisgroupe.net>
2025-02-07 15:37:48 -0500

    Updated code

--- python-project/data_analysis.py
+++ python-project/data_analysis.py
@@ -12,4 +12,5 @@ average_salary = df.groupby("Department")["Salary"].mean()
 print("Average Salary by Department:")
 print(average_salary)
 
+
 average_salary.to_csv("salary_analysis.csv", index=True)
